d2e2049453e7a9d73603985176f415e934389b93
paweskiii <pawel.racki123@gmail.com>
2018-09-25 13:31:45 +0200

    feature/FZ9BF-42-PR-Header - prestyling/prefunctional version

---
+++ src/Header/Header.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { AppBar, Toolbar } from '@material-ui/core';
+
+import SignIn from './SignIn/index';
+import SignUp from './SignUp/index';
+
+import logo from '../assets/logo.png' ;
+
+export default class Header extends Component {
+    render() {
+        return (
+            <AppBar>
+                <Toolbar>
+                    <img src={logo} alt="eat smarter logo" />
+                    <SignIn/>
+                    <SignUp/>
+
+                </Toolbar>
+            </AppBar>
+        )
+    }
+}
