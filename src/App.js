09f9fee1ee8ea9088c83838cd979a506b32b24eb
paweskiii <pawel.racki123@gmail.com>
2018-09-25 15:00:49 +0200

    feature/FZ9BF-42-PR-Header - added simple footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Header from './Header/index';
+import Footer from './Footer/index';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
           <Header/>
+          <Footer/>
       </div>
     );
   }
