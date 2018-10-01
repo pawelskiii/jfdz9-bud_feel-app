be3031235bda404d1f1201295b65b93affd2dd59
patrykfijalkowski <patryk.fijalkowski1@gmail.com>
2018-10-01 23:30:54 +0200

    FZ9BF-28-PF-Carousel-added-carousel-with-demo-picture

--- src/Content/CentralCarousel/Carousel.js
+++ src/Content/CentralCarousel/Carousel.js
@@ -1,73 +1,17 @@
-import React, { Component } from 'react';
-import { Paper, Typography } from '@material-ui/core';
+import React from 'react';
+import Carousel from 'nuka-carousel';
 
-class Carousel extends Component {
+export default class extends React.Component {
     render() {
         return (
-            <Paper>
-                <Typography>
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                    super czad karuzela
-                </Typography>
-            </Paper>
-        )
+            <Carousel>
+                <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide1" />
+                <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide2" />
+                <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide3" />
+                <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide4" />
+                <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide5" />
+                <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide6" />
+            </Carousel>
+        );
     }
 }
-
-export default Carousel;
