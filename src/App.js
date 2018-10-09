78ab8d61860e0c688e0241a7ac36aef837f32722
Marcin Karwacki <kreatywny20@wp.pl>
2018-10-09 15:49:48 +0200

    feature/FZ9BF-44-MK-Some deletions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
 import Header from './Header';
@@ -6,15 +6,15 @@ import Content from './Content';
 import Footer from './Footer';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <Header/>
-          <Content/>
-          <Footer/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Header/>
+                <Content/>
+                <Footer/>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/Content/ContentFees/Fees.js
+++ src/Content/ContentFees/Fees.js
@@ -1,60 +1,12 @@
-import React, { Component } from 'react';
-import { Paper, Typography } from '@material-ui/core';
+import React, {Component} from 'react';
+import {Paper, Typography} from '@material-ui/core';
 
 class Fees extends Component {
     render() {
         return (
             <Paper>
                 <Typography>
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
-                    super czad abonamenty
+
                 </Typography>
             </Paper>
         )
