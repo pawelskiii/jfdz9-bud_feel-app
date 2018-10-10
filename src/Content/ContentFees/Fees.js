d92e72355a028930b2ecfa15c7d95c0eacc931b9
Marcin Karwacki <kreatywny20@wp.pl>
2018-10-10 20:44:16 +0200

    feature/FZ9BF-44-Card styles

--- src/Content/ContentFees/Fees.js
+++ src/Content/ContentFees/Fees.js
@@ -2,11 +2,19 @@ import React, {Component} from 'react';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
+import {withStyles} from '@material-ui/core/styles';
+
+const styles = {
+    card: {
+        maxWidth: 400,
+    },
+};
 
 class Fees extends Component {
     render() {
+        const {classes} = this.props;
         return (
-            <Card>
+            <Card className={classes.card}>
                 <CardActionArea>
                     <CardContent>
                     </CardContent>
@@ -16,4 +24,4 @@ class Fees extends Component {
     }
 }
 
-export default Fees;
+export default withStyles(styles)(Fees);
