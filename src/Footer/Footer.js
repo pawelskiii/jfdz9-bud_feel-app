2033c4ce6d0bacbf08744b87f270de9046117115
Marcin Karwacki <kreatywny20@wp.pl>
2018-10-14 22:28:30 +0200

    feature/FZ9BF-45-MK-Grid footer without styling

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,23 +1,60 @@
 import React, {Component} from 'react';
 import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography'
+import {withStyles} from '@material-ui/core/styles';
+
+const styles = {
+    gridElementCenter: {
+        display: 'flex',
+        justifyContent: 'space-evenly'
+    },
+    content: {
+        textAlign: 'center'
+    },
+    lastElement: {
+        marginBottom: 20
+    },
+};
 
 class Footer extends Component {
     render() {
+        const {classes} = this.props;
 
         return (
-            <Grid container>
-                <Grid item>
-
+            <Grid container className={classes.gridElementCenter}>
+                <Grid item className={classes.content}>
+                    <Typography gutterBottom variant="display1">
+                        Adres:
+                    </Typography>
+                    <Typography gutterBottom variant="subheading">
+                        Bud-Feel
+                    </Typography>
+                    <Typography gutterBottom variant="subheading">
+                        ul. Grunwaldzka 472A
+                    </Typography>
+                    <Typography className={classes.lastElement} variant="subheading">
+                        80-309 Gdańsk
+                    </Typography>
                 </Grid>
-                <Grid item>
-
+                <Grid item className={classes.content}>
+                    <Typography gutterBottom variant="display1">
+                        E-mail:
+                    </Typography>
+                    <Typography className={classes.lastElement} variant="headline">
+                        bud-feel@infoshare.com
+                    </Typography>
                 </Grid>
-                <Grid item>
-
+                <Grid item className={classes.content}>
+                    <Typography gutterBottom variant="display1">
+                        Numer telefonu:
+                    </Typography>
+                    <Typography className={classes.lastElement} variant="headline">
+                        +012 345 6789
+                    </Typography>
                 </Grid>
             </Grid>
         );
     }
 }
 
-export default Footer;
+export default withStyles(styles)(Footer);
