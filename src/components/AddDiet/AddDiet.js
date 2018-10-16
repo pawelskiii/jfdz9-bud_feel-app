9ab1ac2864cdcaea042a1fa893d333740dbbd00c
Marcin Karwacki <kreatywny20@wp.pl>
2018-10-16 15:50:30 +0200

    feature/FZ9BF-30-MK-adding name of diet is working

--- src/components/AddDiet/AddDiet.js
+++ src/components/AddDiet/AddDiet.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
 import Sidebar from '../Sidebar';
 import {withStyles} from '@material-ui/core/styles';
+import { TextField, Button } from "@material-ui/core";
+import firebase from "firebase";
 
 const styles = theme => ({
-    addDietContainer: {
-
-    },
+    addDietContainer: {},
     root: {
         flexGrow: 1,
         zIndex: 1,
@@ -21,7 +21,76 @@ const styles = theme => ({
     toolbar: theme.mixins.toolbar,
 });
 
+// const types = [
+//     {
+//         value: 'masa',
+//         label: 'masa',
+//     },
+//     {
+//         value: 'utrzymanie',
+//         label: 'utrzymanie',
+//     },
+//     {
+//         value: 'redukcja',
+//         label: 'redukcja',
+//     },
+// ];
+//
+// const time = [
+//     {
+//         value: '7',
+//         label: '7 dni',
+//     },
+//     {
+//         value: '14',
+//         label: '14 dni',
+//     },
+//     {
+//         value: '28',
+//         label: '28 dni',
+//     },
+// ];
+
 class AddDiet extends Component {
+
+    state = {
+        id: 999,
+        name: '',
+        description: '',
+        typeId: 1,
+        createdAt: "2018-09-09",
+        weight: {
+            max: 100,
+            min: 10
+        },
+        age: {
+            max: 100,
+            min: 10
+        },
+        period: 7,
+        proposalMeals: {
+            breakfast: '',
+            lunch: '',
+            dessert: '',
+            dinner: '',
+            supper: '',
+        }
+    };
+
+    handleChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+        this.setState({ error: null });
+        firebase.database().ref('/data/diets').push(this.state).catch(
+            error => this.setState({ error })
+        );
+    };
+
     render() {
         const {classes} = this.props;
         return (
@@ -30,11 +99,24 @@ class AddDiet extends Component {
                 <main className={classes.content}>
                     <div className={classes.toolbar}/>
                     <div className={classes.addDietContainer}>
-                        Dodawanie diety
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            id="outlined-name"
+                            label="Nazwa diety"
+                            type="name"
+                            name="name"
+                            variant='outlined'
+                            onChange={this.handleChange}
+                        />
+                        <Button onClick={this.handleSubmit}>
+                            Dodaj dietę
+                        </Button>
                     </div>
                 </main>
             </div>
-        )}
+        )
+    }
 }
 
 
