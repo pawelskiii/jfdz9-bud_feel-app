8cc4d6a76c57661826018c8c196afb8558a6bffb
paweskiii <pawel.racki123@gmail.com>
2018-10-20 09:40:51 +0200

    feature/FZ9BF-28-PR-DetailedDiet sidebar fix

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import Drawer from '@material-ui/core/Drawer';
 import {Grid, Typography, Paper} from '@material-ui/core';
 import {withStyles, MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import amber from '@material-ui/core/colors/amber';
 import blueGrey from '@material-ui/core/colors/blueGrey';
@@ -18,11 +18,19 @@ const styles = theme => ({
     },
     toolbar: theme.mixins.toolbar,
 
-    button: {
-        margin: theme.spacing.unit*2,
+    list: {
+        margin: theme.spacing.unit * 2,
         marginTop: 16,
+        listStyle: 'none',
+        textAlign: 'center'
+    },
+    listContainer: {
+        margin: 0,
+        padding: 0,
+    },
+    button: {
         width: 160
-    }
+    },
 });
 
 
@@ -32,7 +40,6 @@ const theme = createMuiTheme({
         secondary: blueGrey
     }
 });
-
 
 
 function Sidebar(props) {
@@ -47,39 +54,57 @@ function Sidebar(props) {
             }}
         >
             <div className={classes.toolbar}/>
-                           <MuiThemeProvider theme={theme}>
-                               <Link to="/" style={{textDecoration: 'none'}}>
-                                   <Button variant="contained"
-                                           style={{backgroundColor: amber[900], color: blueGrey[800], marginTop: 96}}
-                                           className={classes.button}>
-                                       Strona Główna
-                                   </Button></Link>
-                               <Link to="/UserPanel" style={{textDecoration: 'none'}}>
-                                   <Button variant="contained"
-                                           style={{backgroundColor: amber[800], color: blueGrey[800]}}
-                                           className={classes.button}>
-                                       Moje Dane
-                                   </Button></Link>
-                               <Link to="/Favourites" style={{textDecoration: 'none'}}>
-                                   <Button variant="contained"
-                                           style={{backgroundColor: amber[700], color: blueGrey[800]}}
-                                           className={classes.button}>
-                                       Moje ulubione
-                                   </Button>
-                               </Link>
-                               <Link to="/diets" style={{textDecoration: 'none'}}>
-                                   <Button variant="contained"
-                                           style={{backgroundColor: amber[600], color: blueGrey[800]}}
-                                           className={classes.button}>
-                                       Wybór diet
-                                   </Button></Link>
-                               <Link to="/AddDiet" style={{textDecoration: 'none'}}>
-                                   <Button variant="contained"
-                                           style={{backgroundColor: amber[500], color: blueGrey[800]}}
-                                           className={classes.button}>
-                                       Dodaj dietę
-                                   </Button></Link>
-                           </MuiThemeProvider>
+            <MuiThemeProvider theme={theme}>
+                <ul className={classes.listContainer}>
+                    <li className={classes.list} style={{marginTop: 96}}>
+                        <Link to="/" style={{textDecoration: 'none'}}>
+                            <Button variant="contained"
+                                    style={{backgroundColor: amber[900], color: blueGrey[800]}}
+                                    className={classes.button}>
+                                Strona Główna
+                            </Button>
+                        </Link>
+                    </li>
+                    <li className={classes.list}>
+                        <Link to="/UserPanel" style={{textDecoration: 'none'}}>
+                            <Button variant="contained"
+                                    style={{backgroundColor: amber[800], color: blueGrey[800]}}
+                                    className={classes.button}>
+                                Moje Dane
+                            </Button>
+                        </Link>
+                    </li>
+                    <li className={classes.list}>
+                        <Link to="/Favourites" style={{textDecoration: 'none'}}>
+                            <Button variant="contained"
+                                    style={{backgroundColor: amber[700], color: blueGrey[800]}}
+                                    className={classes.button}>
+                                Moje ulubione
+                            </Button>
+                        </Link>
+                    </li>
+                    <li className={classes.list}>
+                        <Link to="/diets" style={{textDecoration: 'none'}}>
+                            <Button variant="contained"
+                                    style={{backgroundColor: amber[600], color: blueGrey[800]}}
+                                    className={classes.button}>
+                                Wybór diet
+                            </Button>
+                        </Link>
+                    </li>
+                    <li className={classes.list}>
+                        <Link to="/AddDiet" style={{textDecoration: 'none'}}>
+                            <Button variant="contained"
+                                    style={{backgroundColor: amber[500], color: blueGrey[800]}}
+                                    className={classes.button}>
+                                Dodaj dietę
+                            </Button>
+                        </Link>
+                    </li>
+                </ul>
+
+
+            </MuiThemeProvider>
         </Drawer>
     );
 }
