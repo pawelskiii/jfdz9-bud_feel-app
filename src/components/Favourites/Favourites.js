0fc5f9b3f4ee4cae6887f934912d7e00c1971f2a
kursant <you@exam>
2018-10-21 02:51:59 +0200

    FZ9BF-42-PF-Add-to-favourites-code-refactoring

--- src/components/Favourites/Favourites.js
+++ src/components/Favourites/Favourites.js
@@ -1,35 +1,11 @@
 import React, {Component, Fragment} from 'react';
 import Sidebar from '../Sidebar/index';
 import {withStyles} from '@material-ui/core/styles';
-import IconButton from '@material-ui/core/IconButton';
-import FavoriteIcon from '@material-ui/icons/Favorite';
 import {connect} from 'react-redux'
-import {Card, CardActionArea, CardContent, Typography, Grid, Divider, Paper} from '@material-ui/core';
+import {Typography, Paper} from '@material-ui/core';
 import DietTable from '../ContentDiets/DietCards';
 
-import slim3 from '../../assets/slim3.jpeg';
-import mass from '../../assets/mass.jpeg';
-import fit from '../../assets/fit.jpg';
 
-
-/*const styles = theme => ({
-    favContainer: {
-
-    },
-    root: {
-        flexGrow: 1,
-        zIndex: 1,
-        overflow: 'hidden',
-        display: 'flex',
-    },
-    content: {
-        flexGrow: 1,
-        backgroundColor: theme.palette.background.default,
-        padding: theme.spacing.unit * 2,
-        minWidth: 0,
-    },
-    toolbar: theme.mixins.toolbar,
-});*/
 const styles = theme => ({
     panelContainer: {},
     root: {
@@ -53,40 +29,23 @@ class Favourites extends Component {
         favs: ''
     };
 
-/*
-    componentDidMount() {
-
-    }
-*/
-
     getDiets() {
-       console.log(this.props.diets, this.props.favs)
+        console.log(this.props.diets, this.props.favs);
         if (!!this.props.diets && !!this.props.favs) {
-            const favsKeys = Object.keys(this.props.favs)
-           /* debugger;*/
-            console.log("pelna tablica")
+            const favsKeys = Object.keys(this.props.favs);
+            /* debugger;*/
             return this.props.diets.filter(diet => {
                 return favsKeys.includes(diet.id.toString());
-                // return console.log("diety" , el.id);
             });
-            /*Object.keys(this.props.favs).forEach((el) => {
-                console.log("favsy" , el)
-            });*/
         } else {
-            console.log("pusta tablica")
-           return [] ;
+            return [];
         }
-
     }
 
-
     render() {
-
         const {classes, types} = this.props;
 
-
         return (
-
             <Fragment>
                 <div className={classes.root}>
                     <Sidebar/>
@@ -103,7 +62,6 @@ class Favourites extends Component {
     }
 }
 
-
 const mapStateToProps = state => ({
     favs: state.favs === null
         ? {}
@@ -114,7 +72,6 @@ const mapStateToProps = state => ({
     types: state.types === null
         ? {}
         : state.types.data,
-
 });
 
 export default connect(
